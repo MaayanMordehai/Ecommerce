33536b272e1f470ca569827d79ce69e2bdb14601
maayan mordehai <maayanmordehai3@gmail.com>
2022-01-08 22:13:16 +0200

    moved data, designed Product page using react bootstrap

--- src/Components/Product.tsx
+++ src/Components/Product.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import {Card, CardGroup, Button} from 'react-bootstrap';
 
 interface Category {
     name: string,
@@ -13,9 +15,11 @@ interface AdditionalInfo {
 interface Product {
     id: number,
     name: string,
+    imageurl: string,
     uploadedDate: Date,
     price: number,
     description: string,
+    amount: number,
     category: Category,
     sellerName: string,
     additionalInfo: Array<AdditionalInfo>,
@@ -26,8 +30,48 @@ interface ProductProps {
 }
 
 const Product = (props: ProductProps) => {
+  const {product} = props
+
   return (
-    <h1>Product {props.product.name}</h1>
+  <CardGroup>
+    <Card>
+      <Card.Img src={product.imageurl} alt="Card image" />
+      <Card.ImgOverlay/>
+  </Card>
+  <Card>
+    <Card.Body>
+      <Card.Header>
+      <span className="badge bg-success mr-2">{product.category.name}</span>
+      <br/>
+      {product.category.subCategories.map((catagory)=> {
+        return <span className="badge bg-danger mr-2">{catagory}</span>
+      })}
+      </Card.Header>
+      <Card.Title>{product.name}</Card.Title>
+      <Card.Text>
+        {product.description}
+        <br/>
+        <br/>
+        Price : {product.price}$
+        <br/>
+        <br/>
+        <Button variant="outline-primary">Add To 🛒</Button>
+        <br/>
+        <br/>
+        <h5> Additional Information </h5>
+        {product.additionalInfo.map((add) => {
+          return <div>{add.title}: {add.info}</div>
+        })}
+        <br/>
+        <br/>
+        Currently on Stock there are: {product.amount} {product.name}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Uploaded on {product.uploadedDate.getFullYear()}-{product.uploadedDate.getMonth()}-{product.uploadedDate.getDate()}</small>
+    </Card.Footer>
+  </Card>
+  </CardGroup>
   );
 }
 
---
+++ src/Data/index.tsx
@@ -0,0 +1,100 @@
+export const staticProducts = [
+    {
+      id: 1,
+      name: "IPHONE 13 mini",
+      imageurl: "http://cdn.shopify.com/s/files/1/0277/8169/3475/products/back-cover-brown-iphone-13-mini-iphone-decoded-bags-604182.jpg?v=1630416388",
+      description: "The 5.4-inch iPhone 13 mini is the successor to the iPhone 12 mini, while the 6.1-inch iPhone 13 is the replacement for the iPhone 12. Both of the new iPhone 13 models are nearly identical in design to the iPhone 12 models, featuring flat edges, an aerospace-grade aluminum enclosure, a glass back, and a slight increase in thickness (7.65mm). The iPhone 13 models are available in Pink, Blue, Midnight (black), Starlight (silver/gold), and (PRODUCT)RED.",
+      price: 2000,
+      amount: 22,
+      uploadedDate: new Date(2021, 9, 24, 6, 4, 23, 11),
+      category: {
+        name: "Electronic",
+        subCategories: ["Phones", "Apple"]
+      },
+      sellerName: "Apple LTD",
+      additionalInfo: [
+          {
+              title: "Selfie Camera",
+              info: "12 MP, 4K video"
+          },
+          {
+              title: "Main Camera",
+              info: "12 MP, 4K video"
+          },
+          {
+              title: "NPC",
+              info: "yes"
+          },
+          {
+              title: "OS",
+              info: "iOS 15, up to iOS 15.2"
+          }
+      ]
+    },
+    {
+      id: 2,
+      name: "IPHONE 12 Pro Max",
+      imageurl: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MHKK3?wid=1144&hei=1144&fmt=jpeg&qlt=80&.v=1603649004000",
+      description: "The 6.1-inch iPhone 12 pro max is the successor to the iPhone 11 max, while the 5.4-inch iPhone 12 is the replacement for the iPhone 11. Both of the new iPhone 12 models are nearly identical in design to the iPhone 11 models, featuring flat edges, an aerospace-grade aluminum enclosure, a glass back, and a slight increase in thickness (7.65mm). The iPhone 12 models are available in Pink, Blue, Midnight (black), Starlight (silver/gold), and (PRODUCT)RED.",
+      price: 2000,
+      amount: 14,
+      uploadedDate: new Date(2021, 9, 24, 6, 4, 23, 11),
+      category: {
+        name: "Electronic",
+        subCategories: ["Phones", "Apple"]
+      },
+      sellerName: "Apple LTD",
+      additionalInfo: [
+          {
+              title: "Selfie Camera",
+              info: "12 MP, 4K video"
+          },
+          {
+              title: "Main Camera",
+              info: "12 MP, 4K video"
+          },
+          {
+              title: "NPC",
+              info: "yes"
+          },
+          {
+              title: "OS",
+              info: "iOS 15, up to iOS 15.2"
+          }
+      ]
+    },
+    {
+      id: 3,
+      name: "POCOPHONE F2 PRO",
+      imageurl: "https://www.powerplanetonline.com/cdnassets/funda_de_silicona_reinforced_xiaomi_pocophone_f2_pro_06_ad_l.jpg",
+      description: "Xiaomi's PocoPhone is expected to release the Xiaomi PocoPhone F2 soon.\n\nThe smartphone is rumored to come with a 6.67 inches AMOLED display with an in-display fingerprint reader. The screen should be protected by Gorilla Glass 6.\n\nThe smartphone is expected to feature a triple camera setup of 64MP main, 13MP ultra-wide and 8MP telephoto cameras. The front camera should be a 20MP pop-up selfie shooter.\n\nThe phone is said to come in variants such as 6 GB RAM and 8 GB with 128 GB and 256 GB internal storage.\n\nThe phone should feature MIUI V10 (Android 9.0 Pie). ",
+      price: 1000,
+      amount: 36,
+      uploadedDate: new Date(2021, 11, 13, 6, 4, 23, 11),
+      category: {
+        name: "Electronic",
+        subCategories: ["Phones", "Xiaomi"]
+      },
+      sellerName: "Xiaomi",
+      additionalInfo: [
+          {
+              title: "Selfie Camera",
+              info: "12 MP, 4K video"
+          },
+          {
+              title: "Main Camera",
+              info: "12 MP, 4K video"
+          },
+          {
+              title: "NPC",
+              info: "yes"
+          },
+          {
+              title: "OS",
+              info: "MUMI"
+          }
+      ]
+    }
+  ];
+
+export default staticProducts;
